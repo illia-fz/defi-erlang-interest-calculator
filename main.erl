d199386344d18a37071269f5171cd11b9c467eda
Illia <77698519+illia-fz@users.noreply.github.com>
2025-09-30 03:49:16 -0700

    Create main.erl

---
+++ main.erl
@@ -0,0 +1,12 @@
+-module(main).
+-export([compound/3, main/0]).
+
+compound(P, R, N) ->
+    P * math:pow(1 + R, N).
+
+main() ->
+    P = 1000,
+    R = 0.05,
+    N = 5,
+    Amount = compound(P, R, N),
+    io:format("Compound amount: ~p~n", [Amount]).
